d0c2f539b94c237b2e4795cba5aacf917fc3f4f6
Fernanda Martins Nericke <fernandanericke.desu@gmail.com>
2024-04-02 17:56:31 -0300

    adicionando detalhes

--- netflixclone/src/components/Header/index.js
+++ netflixclone/src/components/Header/index.js
@@ -4,7 +4,16 @@ import './Header.css';
 export default() => {
     return(
         <header>
-            ...
+            <div className='header--logo'>
+                <a href="/"> 
+                    <img src="https://logopng.com.br/logos/netflix-94.png"></img>
+                </a>
+            </div>
+            <div className='header--user'>
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"></img>
+                </a>
+            </div>
         </header>
     );
 }
